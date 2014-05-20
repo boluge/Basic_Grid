a60aa95eda03feec333104ccf01a40c3e8002cb4
Stéphane <boluge@gmail.com>
2014-05-20 11:24:46 +0200

    add ico to index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
 		imagemin: {
 			dynamic: {
 				files: [{
@@ -81,6 +82,6 @@ module.exports = function(grunt) {
 	grunt.registerTask('build', ['clean','compass:dist']);
 	grunt.registerTask('dev', ['compass:dev']);
 
-	grunt.registerTask('default', ['dev','connect','imagemin','watch']);
+	grunt.registerTask('default', ['imagemin','dev','connect','watch']);
 
 };
